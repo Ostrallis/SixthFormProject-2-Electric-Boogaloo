0d3f5def9a2560b85a29245fd1885ddaca7227ca
Ostrallis <graciouspanda536@gmail.com>
2024-03-01 21:03:25 +0000

    Did more work on rendering

--- Program.cs
+++ Program.cs
@@ -9,29 +9,37 @@ Start<Program>(new DesktopPlatform());
 
 partial class Program : Simulation
 {
-    
     public Alignment center = Alignment.Center; //Assigns the enum to point to the center value
     public int IDCounter = 0; //Counts the IDs for the nodes
     public List<Node> tempNodeList = new List<Node>(); //List of nodes made
     public List<Edge> tempEdgeList = new List<Edge>();//List of edges
-    public List<Circle> NodeGraphicList = new List<Circle>(); //List of circles created as a result of placing down nodes.
+    public List<Circle> NodeCircleList = new List<Circle>(); //List of circles created as a result of placing down nodes.
 
     //function that is run the frame the window is opened.
     public override void OnInitialize()
     {
+        
         Graph TSMGraph = new Graph();
         Window.Title = "MST Finder";
     }
 
+    public void renderAllObjects(ICanvas canvas) {
+        foreach (Circle circ in NodeCircleList) {
+            canvas.DrawCircle(circ);
+        }
+    }
+
     public override void OnRender(ICanvas canvas)
     {
+        canvas.Clear(Color.White);
+        renderAllObjects(canvas);
         //Checks for LMB being pressed down
         if (Mouse.IsButtonPressed(0)) {
             canvas.Fill(Color.Red); //When creating circles, it will make it so that the circles will be filled.
             Node nodeInstance = new Node(IDCounter);
             Vector2 Pos = Mouse.Position; //Finds vector position of the mouses
             Circle CurCirc = new Circle(Pos, 20, center);
-            canvas.DrawCircle(CurCirc);
+            NodeCircleList.Add(CurCirc);
 
             //After it creates a node, it will create edges connecting that node to the one that was just created.
             foreach (Node cNode in tempNodeList) {
@@ -57,8 +65,6 @@ partial class Program : Simulation
         }
         
         float mouseX, mouseY;
-        canvas.Clear(Color.White);
-        
         mouseX = Mouse.Position.X;
         mouseY = Mouse.Position.Y;
         ImGuiNET.ImGui.Text("Mouse X, Y: ");
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # SixthFormProject
  A project to do with the travelling salesman and bees I guess
+
+I'm also storing what needs to be placed into my coursework document here, well, in terms of screenshots and gifs.
+
+https://gyazo.com/f4d3524a552d2be03d54fc8ec053e1e9 //Only rendering a few frames
